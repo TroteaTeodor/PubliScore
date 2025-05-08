444b4d32ae9ba94e778876ae4d6aeb262ccfd13a
Teodor Trotea <teodor.trotea@student.kdg.be>
2025-05-08 15:57:18 +0200

    starting from 0 due to too many errors

--- areagrade.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd /home/teodortrotea/Documents/AreaGrade && source .venv/bin/activate && python AreaGrade/app.py
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from hackathon-template!")
-
-
-if __name__ == "__main__":
-    main()
